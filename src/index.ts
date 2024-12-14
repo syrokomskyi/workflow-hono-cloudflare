8dc1143d0ad33ca7a17cedbb4bf02374c8cdd308
Andrii Syrokomskyi <signmotion@gmail.com>
2024-12-14 17:03:31 +0100

    Simplified.

--- src/index.ts
+++ src/index.ts
@@ -12,28 +12,15 @@ export default {
   async fetch(req: Request, env: Env): Promise<Response> {
     const url = new URL(req.url);
 
-    // Demo for catching a request to /favicon
-    if (url.pathname.startsWith("/favicon")) {
-      return Response.json({}, { status: 404 });
-    }
-
-    // Get the status of an existing instance, if provided
-    const instanceId = url.searchParams.get("instanceId");
-    if (instanceId) {
-      const instance = await env.MY_WORKFLOW.get(instanceId);
-      return Response.json({
-        status: await instance.status(),
-      });
-    }
-
-    // Spawn a new instance and return the ID and status
-    const instance = await env.MY_WORKFLOW.create();
+    // Spawn a new instance or return the spawned early instance
+    const id = url.searchParams.get("id");
+    const instance = id
+      ? await env.MY_WORKFLOW.get(id)
+      : await env.MY_WORKFLOW.create();
 
     return Response.json({
-      instance: {
-        id: instance.id,
-        details: await instance.status(),
-      },
+      id: instance.id,
+      status: await instance.status(),
     });
   },
 } satisfies ExportedHandler<Env>;
