662ff38554e91b184c89a19f39a8681ccd137b0e
Andrii Syrokomskyi <signmotion@gmail.com>
2024-12-14 16:29:25 +0100

    Added a workflow with durable object.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
-import { DurableObject } from "cloudflare:workers";
+import {
+  DurableObject,
+  WorkflowEntrypoint,
+  type WorkflowEvent,
+  type WorkflowStep,
+} from "cloudflare:workers";
+
+type Env = {
+  // Add your bindings here, e.g. Workers KV, D1, Workers AI, etc.
+  MY_DURABLE_OBJECT: DurableObjectNamespace<MyDurableObject>;
+  MY_WORKFLOW: Workflow;
+};
 
 /**
  * Welcome to Cloudflare Workers! This is your first Durable Objects application.
@@ -15,6 +26,8 @@ import { DurableObject } from "cloudflare:workers";
 
 /** A Durable Object's behavior is defined in an exported Javascript class */
 export class MyDurableObject extends DurableObject {
+  readonly name: string;
+
   /**
    * The constructor is invoked once upon creation of the Durable Object, i.e. the first call to
    * 	`DurableObjectStub::get` for a given identifier (no-op constructors can be omitted)
@@ -22,20 +35,60 @@ export class MyDurableObject extends DurableObject {
    * @param ctx - The interface for interacting with Durable Object state
    * @param env - The interface to reference bindings declared in wrangler.toml
    */
-  // biome-ignore lint/complexity/noUselessConstructor: <explanation>
   constructor(ctx: DurableObjectState, env: Env) {
     super(ctx, env);
+
+    this.name = `DO ${crypto.randomUUID()}`;
   }
 
   /**
    * The Durable Object exposes an RPC method sayHello which will be invoked when when a Durable
    *  Object instance receives a request from a Worker via the same method invocation on the stub
    *
-   * @param name - The name provided to a Durable Object instance from a Worker
    * @returns The greeting to be sent back to the Worker
    */
-  async sayHello(name: string): Promise<string> {
-    return `Hello, ${name}!`;
+  async sayHello(): Promise<string> {
+    return `Hello, ${this.name}!`;
+  }
+}
+
+// Create your own class that implements a Workflow
+export class MyWorkflow extends WorkflowEntrypoint<Env, Params> {
+  // Define a run() method
+  async run(event: WorkflowEvent<Params>, step: WorkflowStep) {
+    // Define one or more steps that optionally return state.
+    const data = await step.do("my first step", async () => {
+      return [1, 2, 3];
+    });
+
+    await step.sleep("wait on something", "20 seconds");
+
+    await step.do(
+      "make a call to write that could maybe, just might, fail",
+      // retry strategy
+      {
+        retries: {
+          limit: 5,
+          delay: "5 second",
+          backoff: "exponential",
+        },
+        timeout: "15 minutes",
+      },
+      async () => {
+        // Do stuff here, with access to the state from our previous steps
+        if (Math.random() > 0.5) {
+          throw new Error("API call to $STORAGE_SYSTEM failed");
+        }
+      },
+    );
+
+    await step.do("my final step with data", async () => {
+      for (const d in data) {
+        // Do something with your state
+      }
+
+      return { completed: data };
+    });
   }
 }
 
@@ -48,19 +101,43 @@ export default {
    * @param ctx - The execution context of the Worker
    * @returns The response to be sent back to the client
    */
-  async fetch(request, env, ctx): Promise<Response> {
+  async fetch(req: Request, env: Env): Promise<Response> {
+    const url = new URL(req.url);
+
+    // Demo for catching a request to /favicon
+    if (url.pathname.startsWith("/favicon")) {
+      return Response.json({}, { status: 404 });
+    }
+
+    // Get the status of an existing instance, if provided
+    const instanceId = url.searchParams.get("instanceId");
+    if (instanceId) {
+      const instance = await env.MY_WORKFLOW.get(instanceId);
+      return Response.json({
+        status: await instance.status(),
+      });
+    }
+
+    // Spawn a new instance and return the ID and status
+    const instance = await env.MY_WORKFLOW.create();
+
     // We will create a `DurableObjectId` using the pathname from the Worker request
     // This id refers to a unique instance of our 'MyDurableObject' class above
-    const id = env.MY_DURABLE_OBJECT.idFromName(new URL(request.url).pathname);
+    const stateId = env.MY_DURABLE_OBJECT.idFromName(url.pathname);
 
     // This stub creates a communication channel with the Durable Object instance
     // The Durable Object constructor will be invoked upon the first call for a given id
-    const stub = env.MY_DURABLE_OBJECT.get(id);
+    const stub = env.MY_DURABLE_OBJECT.get(stateId);
 
-    // We call the `sayHello()` RPC method on the stub to invoke the method on the remote
-    // Durable Object instance
-    const greeting = await stub.sayHello("world");
-
-    return new Response(greeting);
+    return Response.json({
+      instance: {
+        id: instance.id,
+        details: await instance.status(),
+      },
+      state: {
+        id: stateId,
+        name: await stub.sayHello(),
+      },
+    });
   },
 } satisfies ExportedHandler<Env>;
